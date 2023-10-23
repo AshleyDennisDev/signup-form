05350484eb669231536e5e8a20440bc9315ff81a
Ashley Dennis <adenn1852@gmail.com>
2023-10-22 21:55:41 -0400

    form and intro css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,41 @@
-import { Button, TextField } from '@mui/material';
+import { TextField } from '@mui/material';
 import './form.css';
 
 export default function Form() {
   return (
-    <div className="formContainer">
-      <p>Try it free 7 days then $20/month thereafter</p>
+    <div className="formSection">
+      <p className="formBanner"> <strong>Try it free 7 days</strong> then $20/month thereafter</p>
       <form   
-      className='formField'    
+      className='formContainer'    
         noValidate
         autoComplete="off">
-        <div className='formFields'>
+        <div className='formFieldsWrapper'>
         <TextField
+          className='formField'
           id="outlined-required"
           label="First Name"
+          margin="normal"
         />
         <TextField
+          className='formField'
           id="outlined-required"
           label="Last Name"
+          margin="normal"
           />
         <TextField
+          className='formField'
           id="outlined-required"
           label="Email Address"
+          margin="normal"
         />
         <TextField
+          className='formField'
           id="outlined-required"
           label="Password"
+          margin="normal"
         />
         </div>
-        <Button variant="contained">CLAIM YOUR FREE TRIAL</Button>
+        <button className='formButton' variant="contained">CLAIM YOUR FREE TRIAL</button>
         <p>By clicking the button, you are agreeing to our Terms and Services</p>
       </form>
     </div>
